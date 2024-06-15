97f3427bfefcaeb7e8b65015b8ecc0bd2fb21870
Ouma <dennisouma96@gmail.com>
2024-06-15 15:38:42 +0300

    backend deploy

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,7 @@
 const allowedOrigins = require("./allowedOrigin")
 
 const corsOptions = {
-    origin: (origin, callback) => {
+    origin: (callback) => {
         if(allowedOrigins.indexOf(origin) !== -1 || !origin){
             callback(null, true) //error obj, allowed 
         } else {
